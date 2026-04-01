bae9e428779ae22b630476a6f5cd2b0a3ef92284
RinasSam <63971076+RinasSam@users.noreply.github.com>
2026-04-01 17:49:53 +0300

    Preparation for github pages

--- fusion.sh
+++ fusion.sh
@@ -3,6 +3,6 @@
 # fusion.sh: dynamically creates an index.json file for all the mods' description.json files.
 
 printf '{\n\t"mods": [\n' > index.json
-find ./mods/ -type f -name 'description.json' -printf '\t\t"https://github.com/SW2CM/Stick-War-Mods/raw/refs/heads/master/mods/%P",\n' >> index.json
+find ./mods/ -type f -name 'description.json' -printf '\t\t"https://sw2cm.github.io/stick-war-mods/mods/%P",\n' >> index.json
 truncate -s-2 index.json
 printf '\n\t]\n}' >> index.json
